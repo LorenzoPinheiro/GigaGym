c3812f270648e775634bbdee6047746fff867699
Gabsn <gabrieldsn2006@gmail.com>
2025-04-15 16:36:05 -0300

    update
    finalmente terminei uma página!

--- app/src/main/java/com/example/gigagym/routine/StudentRoutineActivity.kt
+++ app/src/main/java/com/example/gigagym/routine/StudentRoutineActivity.kt
@@ -1,21 +1,15 @@
 package com.example.gigagym.routine
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.gigagym.R
 
 class StudentRoutineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_student_routine)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Log.d("bruma", "onCreate Inicializado");
     }
 }
